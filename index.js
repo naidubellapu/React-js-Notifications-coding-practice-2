a55f2fba17b27c2edaab7160d503bdc8aa3b1162
naidubellapu <bellapunaidu@gmail.com>
2023-03-14 13:54:54 +0000

    notifications

---
+++ index.js
@@ -0,0 +1,43 @@
+const Notification = (props) => {
+  const { className, iconUrl, text } = props;
+  const containerClassName = `notification-container ${className}`;
+  return (
+    <div className={containerClassName}>
+      <img className="icon" src={iconUrl} />
+      <p className="text">{text}</p>
+    </div>
+  );
+};
+
+const element = (
+  <div className="notifications-bg-container">
+    <h1 className="heading">Notifications</h1>
+    <div className="notification-list-container">
+      <Notification
+        className="primary-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+        text="Information Message"
+      />
+
+      <Notification
+        className="success-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+        text="Success Message"
+      />
+
+      <Notification
+        className="warning-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+        text="Warning Message"
+      />
+
+      <Notification
+        className="danger-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+        text="Danger Message"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
